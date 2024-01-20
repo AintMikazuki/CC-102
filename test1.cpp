3e910c47169920d582a4dcaf06984a2f2c85386d
AintMikazuki <157221305+AintMikazuki@users.noreply.github.com>
2024-01-21 02:01:11 +0800

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() 
+{
+    int number = 0;
+    int sum = 0;
+   
+    do {
+        
+        sum += number;
+    
+        cout << "Enter a number: ";
+        cin >> number;
+
+    }   while (number >= 0);
+    
+    cout << endl << "The sum is " << sum << endl;
+    
+    return 0;
+}
